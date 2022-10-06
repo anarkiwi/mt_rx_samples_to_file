6356b8f1486486c0f0629676e4821a549f03badc
Josh Bailey <josh@vandervecken.com>
2022-10-06 19:16:05 +1300

    smoke test.

--- sample_pipeline_test.cpp
+++ sample_pipeline_test.cpp
@@ -1,16 +1,11 @@
 #define BOOST_TEST_MAIN
-
 #include <boost/test/unit_test.hpp>
-
 #include "sample_pipeline.h"
 
-BOOST_AUTO_TEST_CASE(MyTestCase)
-{
-    // To simplify this example test, let's suppose we'll test 'float'.
-    // Some test are stupid, but all should pass.
-    float x = 9.5f;
 
-    BOOST_CHECK(x != 0.0f);
-    BOOST_CHECK_EQUAL((int)x, 9);
-    BOOST_CHECK_CLOSE(x, 9.5f, 0.0001f); // Checks differ no more then 0.0001%
+BOOST_AUTO_TEST_CASE(SmokeTest)
+{
+    init_sample_buffers(1e3*1024, 4);
+    sample_pipeline_start("short", "", "", 1, false, 0, 0, 1, 1, 1e6, 0, 0);
+    sample_pipeline_stop(0);
 }
