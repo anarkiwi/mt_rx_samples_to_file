f5845721719bb81ba316f3034ffd452565990fe1
Josh Bailey <josh@vandervecken.com>
2022-10-24 16:06:02 +1300

    reset not close.

--- sample_writer.cpp
+++ sample_writer.cpp
@@ -66,8 +66,7 @@ void SampleWriter::open(const std::string &file, size_t zlevel) {
 void SampleWriter::close(size_t overflows) {
     if (outfile.is_open()) {
 	std::cerr << "closing " << file_ << std::endl;
-        outfile.close();
-	boost::iostreams::close(*outbuf_p);
+        outbuf_p->reset();
 
 	if (overflows) {
 	    std::string dirname(boost::filesystem::canonical(orig_path_.parent_path()).c_str());
